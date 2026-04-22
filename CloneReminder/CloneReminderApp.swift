1fdd333b609c91f1e354434729d702927c7b7ae3
Kaique Magno <kaique.magno@hotmail.com>
2026-04-22 14:38:28 -0300

    Initial Commit

---
+++ CloneReminder/CloneReminderApp.swift
@@ -0,0 +1,32 @@
+//
+//  CloneReminderApp.swift
+//  CloneReminder
+//
+//  Created by Kaique Magno on 22/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CloneReminderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CloneReminderTests/CloneReminderTests.swift
@@ -0,0 +1,19 @@
+//
+//  CloneReminderTests.swift
+//  CloneReminderTests
+//
+//  Created by Kaique Magno on 22/04/26.
+//
+
+import Testing
+@testable import CloneReminder
+
+struct CloneReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
